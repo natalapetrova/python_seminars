07cc71dc0397fcd59c9b69c86fb8284b2c47ac41
Natali Petrova <natalapetrova@bk.ru>
2022-10-11 13:27:57 +0300

    add task 7

--- seminar_1/task_7.py
+++ seminar_1/task_7.py
@@ -1,6 +1,9 @@
 # 7 Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
 # для всех значений предикат.
 
+# from turtle import right
+
+
 x = input('Введите первое значение: ')
 y = input('Введите второе значение: ')
 z = input('Введите третье значение: ')
@@ -9,3 +12,16 @@ if not(x or y or z) == (not(x) and not(y) and not(z)):
     print('Утверждение верно')
 else:
     print('Утверждение ложно')
+
+# Второй вариант решения
+
+def logical(x,y,z):
+    left = not (x or y or z)
+    right = not x and not y and not z
+    return left == right
+
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            print(f"x {x} y {y} z {z}")
+            print(logical(z,y,z))
